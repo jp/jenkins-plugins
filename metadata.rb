0a4afe1e43ec17a6b62084e0ef9587de663fd480
Julien Pellet <pelletj@gmail.com>
2014-03-05 17:58:48 +0100

    init plugins

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'jenkins-plugins'
+maintainer       'JP'
+maintainer_email 'chef@julienpellet.com'
+license          'All rights reserved'
+description      'Installs/Configures jenkins plugins'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
